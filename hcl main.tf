9be69fbbcbe3919553b40c3b92cb6c55e3e04ba8
Eduardo García Madruga <egmadruga@gmail.com>
2025-11-17 13:51:58 +0100

    Add commented azurerm backend config to main.tf
    Introduced a commented-out Terraform backend block for AzureRM to guide future remote state configuration. This serves as a template for setting up remote state storage once the required Azure resources are available.

--- hcl main.tf
+++ hcl main.tf
@@ -1,3 +1,13 @@
+terraform {
+  # Uncomment this block after creating a storage account for remote state
+  # backend "azurerm" {
+  #   resource_group_name  = "terraform-state-rg"
+  #   storage_account_name = "tfstatexxxxx"  # Must be globally unique
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "13b98216-b11e-482f-b0a8-af5294c9f076"
